0e79f4ae6555ec6be8fed7cb9b15ccb177fb74c1
s18289 <s18289@pjwstk.edu.pl>
2020-03-24 22:41:23 +0100

    commit

---
+++ WebApplication1/DAL/MockDbService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+                new Student { IdStudent = 1, FirstName = "Jan", LastName = "Kowalski", IndexNumber = "s15236" },
+                new Student { IdStudent = 2, FirstName = "Anna", LastName = "Malewski", IndexNumber = "s17945" },
+                new Student { IdStudent = 3, FirstName = "Andrzej", LastName = "Kotowski", IndexNumber = "s13865" }
+            };
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
